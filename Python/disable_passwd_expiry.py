463512e4cdc002f5b61e1063c0d1ff07e130ce3d
JD <jd@jdfant.com>
2022-09-28 21:06:37 -0600

    format

--- Python/disable_passwd_expiry.py
+++ Python/disable_passwd_expiry.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
-""" no error checks, just a quick way to disable a user's password exiry """
+"""
+no error checks, just a quick way to disable a user's password exiry
+"""
 import os
 import sys
 import subprocess
 
 USER_NAME = "netadmin"
 
+
 def root():
-    '''Are we root?'''
+    """Are we root?"""
     user = os.getuid()
     if user == 0:
-        print "\n Do not run this as root.\n"
+        print("\nDo not run this as root.\n")
         subprocess.call('sudo logger -t FAIL "root attempt to execute ?@"', shell=True)
         sys.exit()
     else:
-        print"\n"
+        print("\n")
+
 
 root()
 
-if __name__ == '__main__':
-    print "\n Here is the Existing password expiry information for '%s'\n" % (USER_NAME)
+if __name__ == "__main__":
+    print("\nHere is the Existing password expiry information for '%s'\n") % (USER_NAME)
     subprocess.call("sudo /usr/bin/chage -l '%s'" % (USER_NAME), shell=True)
-    print "\n Here is the UPDATED password expiry information for %s\n"
-    subprocess.call("sudo /usr/bin/chage -I -1 -m0 -M99999 -E -i '%s'" % (USER_NAME), shell=True)
-    print "\n Please check for accuracy \n"
+    print("\n Here is the UPDATED password expiry information for %s\n")
+    subprocess.call(
+        "sudo /usr/bin/chage -I -1 -m0 -M99999 -E -i '%s'" % (USER_NAME), shell=True
+    )
+    print("\nPlease check for accuracy\n")
